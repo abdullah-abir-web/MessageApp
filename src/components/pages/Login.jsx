845da00ec415429e1b2354467e300c27a6e3c151
unknown <abdullahabirweb10@gmail.com>
2024-03-31 21:43:39 +0600

    login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,52 +1,94 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
 function Login() {
   const auth = getAuth();
-  // const [userError, setUserError] = useState({
-  //   emailError: "",
-  //   passwordError: "",
-  // });
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
-  const handelSubmit = () => {};
-  // if (!loginData .email) {
-  //   setUserError({ emailError: "Email is Required!" });
-  // } else if (!loginData.password) {
-  //   setUserError({ passwordError: "password is Required!" });
-  // }
+  const handelSubmit = () => {
+    if (!loginData.email) {
+      setEmailError("Email is Required!");
+    } else if (!loginData.password) {
+      setPasswordError("password is Required!");
+    } else {
+      signInWithEmailAndPassword(auth, loginData.email, loginData.password)
+        .then((res) => {
+          console.log("login Succes", res);
+        })
+        .catch((err) => {
+          console.log(err.code);
+          if (err.code == "auth/invalid-email") {
+            setEmailError("Invalid Email! Please input a valid email");
+          }
+          if (err.code == "auth/invalid-credential") {
+            toast.error("Authorization faild!", {
+              position: "top-center",
+              autoClose: 2000,
+              closeOnClick: true,
+              theme: "light",
+            });
+          }
+          if (err.code == "auth/too-many-requests") {
+            toast.error(
+              "Too many request! user temporarily block pleare try agail later or reset your password",
+              {
+                position: "top-center",
+                autoClose: 2000,
+                closeOnClick: true,
+                theme: "light",
+              }
+            );
+          }
+        });
+    }
+  };
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-common">
+    <div className="flex items-center justify-center h-screen bg-common">
+      <ToastContainer />
       <div className='"w-full max-w-md bg-white rounded-lg shadow-md p-10'>
         <h2 className="font-primary font-bold text-3xl mb-4">Sign In</h2>
-        <form action="" className="flex flex-col">
+        <div className="flex flex-col">
           <input
-            onChange={(e) =>
-              setLoginData({ ...loginData, email: e.target.value })
-            }
+            onChange={(e) => {
+              setLoginData({ ...loginData, email: e.target.value }),
+                setEmailError("");
+            }}
             className="bg-common text-black border-0 rounded-md p-2 mb-4 outline-none"
             type="email"
-            placeholder="Email address"
+            placeholder="Email"
           />
-
+          {emailError && (
+            <p className="text-primary text-sm text-start font-secondary font-medium">
+              {emailError}
+            </p>
+          )}
           <input
-            onChange={(e) =>
-              setLoginData({ ...loginData, password: e.target.value })
-            }
+            onChange={(e) => {
+              setLoginData({ ...loginData, password: e.target.value }),
+                setPasswordError("");
+            }}
             className="bg-common text-black border-0 rounded-md p-2 mb-4 outline-none"
             type="password"
             placeholder="Password"
           />
+          {passwordError && (
+            <p className="text-primary text-sm text-start font-secondary font-medium">
+              {passwordError}
+            </p>
+          )}
 
           <div className=" flex items-center justify-between flex-wrap">
             <label
               htmlFor="remember-me"
-              className=" text-sm text-black font-primary cursor-pointer font-semibold"
+              className=" text-lg text-black font-primary cursor-pointer font-semibold mt-2"
             >
-              <input className="mr-2" id="remember-me" type="checkbox" />
-              Remember me
+              <input className="mr-2" type="checkbox" />
+              Remember Me
             </label>
             <Link
               className="text-sm text-primary font-primary font-medium  mb-.5 "
@@ -71,7 +113,7 @@ function Login() {
           >
             Sign In
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
