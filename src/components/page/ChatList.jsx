29d580d8066b6facbd73a48bf43d44102ba936b8
unknown <abdullahabirweb10@gmail.com>
2024-04-27 22:07:57 +0600

    first commit

---
+++ src/components/page/ChatList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ChatList = () => {
+  return (
+    <div className="flex gap-4 bg-common cursor-pointer">
+    <div>
+      <img src="/user.png" alt="user" width={50} />
+    </div>
+    <div>
+      <h2 className="font-semibold  text-xl font-secondary">Abdullah Abir</h2>
+      <p className=" font-regular text-secondary text-sm font-secondary">Need Money.....</p>
+    </div>
+    <p className=" ml-auto font-regular text-secondary font-secondary text-3">10:30 PM</p>
+  </div>
+  )
+}
+
+export default ChatList
--- src/components/page/Chatbox.jsx
+++ src/components/page/Chatbox.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import ChatList from "./ChatList";
+// import { FaSearch } from "react-icons/fa";
 const Chatbox = () => {
   return (
-    <div>Chatbox</div>
-  )
-}
+    <div className="flex h-screen w-full">
+      <div className="h-full bg-common flex flex-col gap-4 p-5">
+        <h2 className="text-2xl font-semibold font-secondary pb-4">Chat</h2>
 
-export default Chatbox
+        <ChatList />
+        <ChatList />
+        <ChatList />
+        <ChatList />
+        <ChatList />
+      </div>
+      <div className="h-full">Message</div>
+    </div>
+  );
+};
+
+export default Chatbox;
