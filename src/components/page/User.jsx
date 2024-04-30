7e84d2c38d9cd8c349bb7b2944c5ea6bda3375f1
unknown <abdullahabirweb10@gmail.com>
2024-04-30 19:29:09 +0600

    button

--- src/components/page/User.jsx
+++ src/components/page/User.jsx
@@ -4,72 +4,94 @@ import { MdEmail } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
 import { BiSolidEditAlt } from "react-icons/bi";
 import { MdMoreVert } from "react-icons/md";
-// import Cropper from "react-cropper";
-// import "cropperjs/dist/cropper.css";
-// import { createRef,useState } from "react";
 import { useSelector } from "react-redux";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { createRef, useState } from "react";
+import { GiCrossMark } from "react-icons/gi";
+
 const User = () => {
   const user = useSelector((state) => state.userSlice.user);
+  const [enableEdit, setEnableEdit] = useState(false);
+  const [image, setImage] = useState();
+  const [CropData, setCropData] = useState("");
+  const cropperRef = createRef();
+  const onChange = (e) => {
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
 
-  // const [image, setImage] = useState("https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg");
-  // const [CropData, setCropData] = useState("");
-  // const cropperRef = createRef();
-  // const onChange = (e) => {
-  //   let files;
-  //   if (e.dataTransfer) {
-  //     files = e.dataTransfer.files;
-  //   }
-  //   else if (e.target) {
-  //     files = e.target.files;
-  //   }
-  //   const reader = new FileReader();
-  //   reader.onload = () => {
-  //     setImage(reader.result);
-  //   };
-  //   reader.readAsDataURL(files[0]);
-  // };
-
-  // const getCropData = () => {
-  //   if (typeof cropperRef.current?.cropper !== "undefined") {
-  //     setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
-  //   }
-  // };
-
-  // console.log(CropData);
-
+  const getCropData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+    }
+  };
+  const handelClose = () => {
+    setEnableEdit(false);
+    setCropData("");
+    setImage("");
+  };
   return (
-    <div className="w-80  bg-white shadow-lg rounded-lg overflow-hidden my-4 m-auto h-fit">
-      {/* <div class="file-upload">
-  <div class="file-select">
-    <div class="file-select-button" id="fileName">Choose File</div>
-    <div class="file-select-name" id="noFile">No file chosen...</div> 
-    <input type="file" name="chooseFile" id="chooseFile"/>
-  </div>
-</div> */}
+    <div className="w-80  bg-white shadow-lg rounded overflow-hidden my-4 m-auto h-fit ">
+      {enableEdit && (
+        
+        <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.6)] border p-5 rounded flex justify-center items-center">
+        <div className=" p-5 rounded  w-1/4 ">
+        <div onClick={handelClose} className="flex justify-between my-2 ">
+            <button  className="bg-primary text-white font-secondary font-semibold p-2 rounded">
+              Close
+            </button>
+            <button className="bg-[#008000] text-white font-secondary font-semibold p-2 rounded">
+              save
+            </button>
+          </div>
+        <input
+            className="p-2 bg-gray-900 text-white  w-full font-secondary"
+            type="file"
+            onChange={onChange}
+          />
+          {image && (
+            <Cropper
+              ref={cropperRef}
+              style={{ height: 400, width: "100%" }}
+              zoomTo={0.5}
+              initialAspectRatio={1}
+              preview=".img-preview"
+              src={image}
+              viewMode={1}
+              minCropBoxHeight={10}
+              minCropBoxWidth={10}
+              background={false}
+              responsive={true}
+              autoCropArea={1}
+              checkOrientation={false}
+              guides={true}
+            />
+          )}
+          <button
+            onClick={getCropData}
+            className="crop-btn my-2 font-secondary "
+          >
+            Crop Image
+          </button>
+          <img src={CropData} alt="" className="w-20" />
+        </div>
+        </div>
+      )}
 
-      {/* <div className="my-20 ">
-        <input type="file"  onChange={onChange}/ >
-        <Cropper
-          ref={cropperRef}
-          style={{ height: 400, width: "100%" }}
-          zoomTo={0.5}
-          initialAspectRatio={1}
-          preview=".img-preview"
-          src={image}
-          viewMode={1}
-          minCropBoxHeight={10}
-          minCropBoxWidth={10}
-          background={false}
-          responsive={true}
-          autoCropArea={1}
-          checkOrientation={false} 
-          guides={true}
-        />
-      </div> */}
       <img
         className="w-full h-60 object-cover object-center"
-        src={user?.photoURL}
-        alt="avatar"
+        src={ user?.photoURL}
+        alt=""
       />
       <div className="flex items-center justify-between  px-6 py-3 bg-gray-900">
         <h1 className="mx-3 text-white font-semibold text-lg font-secondary">
@@ -78,9 +100,12 @@ const User = () => {
         <div>
           <div className="group relative w-fit">
             <MdMoreVert className="text-white text-2xl cursor-pointer" />
-            <p className="text-white font-secondary hidden absolute bottom-full right-0 group-hover:block whitespace-nowrap">
+            <button
+              onClick={() => setEnableEdit(true)}
+              className=" text-white font-secondary hidden absolute bottom-full right-0 group-hover:block whitespace-nowrap"
+            >
               Edit Profile
-            </p>
+            </button>
           </div>
         </div>
       </div>
